c0e0aeb293117edaf5e601b53ea6fcbde6a72e3d
Alexandra <sana201193@gmail.com>
2024-04-03 21:18:20 +0700

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    int calculate(double heightMetres, double weightKG){
-        double bmi = weightKG/(heightMetres*heightMetres);
+    int calculate(double heightMetres, double weightKG) {
+        double bmi = weightKG / (heightMetres * heightMetres);
         return (int) bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double height = 1.87;
         double weight = 98;
-        int bmi = service.calculate(height,weight);
+        int bmi = service.calculate(height, weight);
         System.out.println(bmi);
     }
 }
